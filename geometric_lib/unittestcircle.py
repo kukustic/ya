63e5c652d5abd141cfd7ac6df206dbfd8afb684b
kukustic <144245777+kukustic@users.noreply.github.com>
2023-12-13 16:03:57 +0300

    Update unittestcircle.py

--- geometric_lib/unittestcircle.py
+++ geometric_lib/unittestcircle.py
@@ -8,13 +8,6 @@ class CircleTestCase(unittest.TestCase):
         self.assertEqual(areacir, 0)
         self.assertEqual(perimetercir, 0)
 
-    def test_per_cir(self):
-        perimetercir = perimeter(20)
-        self.assertEqual(perimetercir, 125.66370614359172)
-
-    def test_area_cir(self):
-        areacir = area(17)
-        self.assertEqual(areacir, 907.9202768874503)
 
 if __name__ == '__main__':
     unittest.main()
